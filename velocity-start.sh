d765f9ad34a9ad22e2c6f8d491803bd75d1fdbbc
Jonathan Thimesch <JRThimesch@gmail.com>
2024-08-25 17:58:50 -0500

    base config

--- velocity-start.sh
+++ velocity-start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+version="2"
 echo 'Starting Velocity proxy...'
+echo "Version: $version"
+
 java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar /velocity/velocity*.jar

a6170a94ec274605b71da7c2fcc3a071f3fd8fc6
Jonathan Thimesch <JRThimesch@gmail.com>
2024-08-25 20:56:01 -0500

    base config

--- velocity-start.sh
+++ velocity-start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-version="9"
+version="10"
 echo 'Starting Velocity proxy...'
 echo "Version: $version"
 
-java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar ./velocity*.jar &
-
-# Echo the forwarding.secret file
-echo "$(cat forwarding.secret)"
-
-# Bring the Java process back to the foreground
-fg %1
+java -Xms1G -Xmx1G -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar ./velocity*.jar
